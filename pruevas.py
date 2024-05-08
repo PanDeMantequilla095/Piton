fd07b4866bc6b9f3a52be8d3a94ee7cb7db38a37
Vanne <111906427+PanDeMantequilla095@users.noreply.github.com>
2024-05-08 14:55:26 -0600

    fotos del programa feo

---
+++ pruevas.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+#C:\\Users\\vanne\\OneDrive\\Documentos\\Harkness\\asesorias\\hermes\\pes\\eris.ico
+
+raiz=Tk()
+raiz.title("pirulin")
+img=PhotoImage(file="C:\\Users\\vanne\\OneDrive\\Documentos\\Harkness\\asesorias\\hermes\\pes\\erisfin.png")#no debe de pasar los 50x50 pixeles :C
+raiz.iconphoto(False,img)
+raiz.mainloop()
